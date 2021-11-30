241959a012bdacedfd40eac4a8adaf2fd24ccfb6
Abed Al Qader Madi <80624286+Madii-01@users.noreply.github.com>
2021-11-30 16:36:33 +0200

    madii

--- Coffee_House/JS/Script.js
+++ Coffee_House/JS/Script.js
@@ -14,3 +14,71 @@ document.querySelector('#search-btn').onclick = () =>{
     cartItem.classList.remove('active');
 }
 
+let carts = document.querySelectorAll('.add-btn');
+
+
+for(let i=0; i<carts.length; i++){
+    carts[i].addEventListener('click' , () => {
+        cartNumbers();
+    })
+}
+
+let menuItems = [
+    {
+        name: 'AMERICAN COFFEE',
+        tag: 'AMERICAN COFFEE',
+        price: 2.99,
+        incart: 0 
+    },
+    {
+        name: 'CARAMEL MACCHIATO',
+        tag: 'CARAMEL MACCHIATO',
+        price: 3.99,
+        incart: 0 
+    },
+    {
+        name: 'ICED SPANISH LATTE',
+        tag: 'ICED SPANISH LATTE',
+        price: 3.99,
+        incart: 0 
+    },
+    {
+        name: 'SPANISH LATTE',
+        tag: 'SPANISH LATTE',
+        price: 3.99,
+        incart: 0 
+    },
+    {
+        name: 'TURKISH COFFEE',
+        tag: 'TURKISH COFFEE',
+        price: 3.99,
+        incart: 0 
+    },
+    {
+        name: 'WHITE MOCHA',
+        tag: 'WHITE MOCHA',
+        price: 3.99,
+        incart: 0 
+    },
+]
+    
+
+
+
+function cartNumbers() {
+    let menuPorducts = localStorage.getItem('cartNumbers');
+    
+    menuPorducts =  parseFloat(menuPorducts);
+
+    if(menuPorducts){
+        localStorage.setItem('cartNumbers' , menuPorducts + 1);
+        
+    }
+    else{
+        localStorage.setItem('cartNumbers', 1);
+     
+
+    }
+    
+
+}
