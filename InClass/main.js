7623b299b26150868df1c2c09ccf3ebabd18e9ad
Abed Al Qader Madi <80624286+Madii-01@users.noreply.github.com>
2021-12-30 12:59:29 +0200

    masdii

--- InClass/main.js
+++ InClass/main.js
@@ -1,8 +1,18 @@
 const buy = document.querySelectorAll(".btn")[0];
+const madii = document.querySelectorAll(".btn") [1];
 const biggerContainer = document.querySelector(".bigger-contaier")
 buy.addEventListener("click" , (eo) => {
 
-biggerContainer.classList.add("active")
+    biggerContainer.classList.add("active")
 
+});
 
-})
+madii.addEventListener("click" , (eo) =>{
+
+    biggerContainer.classList.remove("active")
+});
+const you = document.getElementById("you")
+you.addEventListener("click", (eo) => {
+eo.preventDefault();
+  
+});
