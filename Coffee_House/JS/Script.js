882244061629242bab29be8d943e67e86b636820
Abed Al Qader Madi <80624286+Madii-01@users.noreply.github.com>
2022-01-23 15:47:24 +0200

    madi

--- Coffee_House/JS/Script.js
+++ Coffee_House/JS/Script.js
@@ -169,7 +169,7 @@ function displayCart(){
             <div class = " basketTotalContainer">
                 <h4 class = "basketTotalTitle">Basket Total:</h4>
                 <h4 class="basketTotal">$${cartCost}</h4>
-                <a href="check-out.html" target="blank" class="check-out-btn" > CHECK OUT</a>
+                <a href="check-out.html" target="_blank"  class="check-out-btn" > CHECK OUT</a>
             </div>`
 
             
